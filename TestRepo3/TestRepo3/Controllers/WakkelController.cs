60d359cc80b9f9db98d0338548c70bb5ed5b1b76
ahmedlwakeel <40790294+ahmedlwakeel@users.noreply.github.com>
2018-07-09 11:44:52 +0200

    Enter commit message

---
+++ TestRepo3/TestRepo3/Controllers/WakkelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestRepo3.Controllers
+{
+    public class WakkelController : Controller
+    {
+        //
+        // GET: /Wakkel/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
