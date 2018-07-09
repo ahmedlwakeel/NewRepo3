1986e196bf3066b20591d02f0c29152d04246188
ahmedlwakeel <40790294+ahmedlwakeel@users.noreply.github.com>
2018-07-09 14:53:31 +0200

    create  file TestBController

---
+++ TestRepo3/TestRepo3/Controllers/TestBController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestRepo3.Controllers
+{
+    public class TestBController : Controller
+    {
+        //
+        // GET: /TestB/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
