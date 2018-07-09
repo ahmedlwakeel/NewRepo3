8ca5e92c8d4aac2cb1686429a626264a686f390b
Hanan <hanan.wahab@hindawi.com>
2018-07-09 14:49:22 +0200

    create TestAController
    and .gitignore file

---
+++ TestRepo3/TestRepo3/Controllers/TestAController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestRepo3.Controllers
+{
+    public class TestAController : Controller
+    {
+        //
+        // GET: /TestA/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
